b86962801cf5e07fc86a9c77ba072d8e08b93394
m4tta <mattandersonj@gmail.com>
2018-06-06 02:33:24 -0700

    not found page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="min-h-screen bg-green-light flex flex-col">
+  <Header />
+  <div class="flex flex-col bg-green-light w-full">
+    <div class="flex flex-col items-center h-full sm:py-16">
+      Not Found
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import Header from '@/components/Header.vue'
+
+export default {
+  name: 'NotFound',
+  components: {
+    Header,
+  },
+}
+</script>
+
+<style scoped>
+</style>
