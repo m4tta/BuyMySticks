fb08d0e39e23ae64fcda389e6678f3d262943326
m4tta <mattandersonj@gmail.com>
2018-06-06 02:05:35 -0700

    unmount and destroy stripe

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -177,6 +177,10 @@ export default {
     card = elements.create('card', {style: this.stripeOptions.style})
     card.mount(this.$refs.card)
   },
+  beforeDestroy() {
+    card.unmount()
+    card.destroy()
+  },
   computed: {
     // ...mapState(['products']),
     states: () => ["Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut", "District of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", " North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"],
